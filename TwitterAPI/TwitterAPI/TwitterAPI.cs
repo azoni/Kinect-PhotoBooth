44d3bc27d03d3846e1b6c200f3c90f09bdffd8cf
RhaydenX <alamb25@uw.edu>
2017-04-01 15:17:36 -0700

    tweets

---
+++ TwitterAPI/TwitterAPI/TwitterAPI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
+
+namespace FaceBooth
+{
+    
+    class TwitterAPI
+    {
+        
+        static void TweetPicture(string path)
+        {
+            var credentials = new TwitterCredentials("ZPelDIgzmbLcpxB4qqOwrG52U",
+                                                     "0mRtHj3gF1sakk3V22gafM4EVttn3v5QrGbwkkdyjyA0hYxAOe",
+                                                     "848276891693862912-kvPEuoUKboZzp9H4tcqgtYEnE5wAgUv",
+                                                     "uy2Xgq2aBBtMDt5yE6NKQa7BwDJrvabckbJuUiZCBwvbR");
+            Auth.SetCredentials(credentials);
+
+            byte[] file1 = File.ReadAllBytes(path);
+            var media = Upload.UploadImage(file1);
+
+            var tweet = Tweet.PublishTweet("LA HACKS 2017!", new PublishTweetOptionalParameters
+            {
+                Medias = new List<IMedia> { media }
+            });
+
+        }
+    }
+}
